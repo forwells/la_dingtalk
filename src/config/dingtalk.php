6cefedea942ee0174e3568b574d2b7aca1c33ef7
yadan <demo@demo.com>
2022-06-22 16:34:06 +0800

    update

--- src/config/dingtalk.php
+++ src/config/dingtalk.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'agent_id' => '1689445496',
-    'mini_app_id' => '5000000002718146',
-    'app_key' => 'dingkpyh8re8f7vybkkd',
-    'app_secret' => 'VnlW5vlV5TBY9v7DxRwuvtqOIE7WgbZRxwNuMjOAqkYoKloRyfKVp3nCVgPUu76O'
+    'agent_id' => '',
+    'mini_app_id' => '',
+    'app_key' => '',
+    'app_secret' => ''
 ];
